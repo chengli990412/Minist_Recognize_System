1e74f39d4268b984acecf84d9316728c7599f7be
程力 <cheng425863@icloud.com>
2023-02-12 21:50:15 +0800

    版本：1.0.0.5 新增Myclass类负责全局管理

--- Deeplearning.py
+++ Deeplearning.py
@@ -1,3 +1,7 @@
+'''
+深度学习类
+'''
+
 import numpy as np
 import os
 import gzip
@@ -6,80 +10,96 @@ from tensorflow import keras
 from keras import layers, optimizers, datasets
 import tensorboard as tb
 
-# #深度学习类
-class DeepLearningClass:
-    def load_data(data_folder):
-        files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz',
+
+# #深度学习静态类
+
+image_train = ()
+label_train = ()
+image_test = ()
+label_test = ()
+
+def load_data()->str:
+    global image_train,image_test,label_train,label_test
+    files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz',
                  't10k-images-idx3-ubyte.gz']
-        paths = []
-        for fname in files:
-            paths.append(os.path.join(data_folder, fname))
-        if len(paths) == 0:
-            return "NG"
+    paths = []
+    for fname in files:
+        paths.append(os.path.join('MNIST/', fname))
+    if len(paths) == 0:
+        return '路径异常，请检测路径设置是否正确'
 
-        # 打开文件
-        with gzip.open(paths[0], 'rb') as lbpath:
-            y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-        with gzip.open(paths[1], 'rb') as imgpath:
-            x_train = np.frombuffer(
-                imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28)
-        with gzip.open(paths[2], 'rb') as lbpath:
-            y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-        with gzip.open(paths[3], 'rb') as imgpath:
-            x_test = np.frombuffer(
-                imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28)
+    with gzip.open(paths[0], 'rb') as lbpath:
+            label_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    with gzip.open(paths[1], 'rb') as imgpath:
+            image_train = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(label_train), 28, 28)
 
-        return (x_train, y_train), (x_test, y_test)
+    with gzip.open(paths[2], 'rb') as lbpath:
+            label_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+    with gzip.open(paths[3], 'rb') as imgpath:
+            image_test = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(label_test), 28, 28)
+
+    return 'OK'
+    #创建模型
+
+def CreateModel():
+    model = keras.Sequential([layers.Dense(256, activation='relu'),
+                                  layers.Dense(128, activation='relu'),
+                                  layers.Dense(10)])
+    model.build(input_shape=(None, 28 * 28))
+    model.summary()
+    return model
+    #加载模型
+
+def LoadModel(path):
+    new_model = keras.models.load_model('net_model.h5')
+    return new_model
+
+def SaveModel(model,path):
+    model.save(path)
+    #训练
+
+def Dl_Run(train_images, train_labels,MainWindow:object):
+    x = tf.convert_to_tensor(train_images, dtype=tf.float32) / 255
+    db = tf.data.Dataset.from_tensor_slices((x, train_labels))
+    db = db.batch(100).repeat(20)
+    optimizer = optimizers.SGD(lr=0.02)
+    acc_meter = keras.metrics.Accuracy()
+    summary_writer = tf.summary.create_file_writer('tf_log')
+    text = ''
+    for step, (xx, yy) in enumerate(db):
+
+        with tf.GradientTape() as tape:
+            # 图像样本大小重置(-1, 28*28)
+            xx = tf.reshape(xx, (-1, 28 * 28))
+            # 获取输出
+            out = model(xx)
+            # 实际标签转为onehot编码
+            y_onehot = tf.one_hot(yy, depth=10)
+            # 计算误差
+            loss = tf.square(out - y_onehot)
+            loss = tf.reduce_sum(loss / xx.shape[0])
+        # 更新准备率
+        acc_meter.update_state(tf.argmax(out, axis=1), yy)
+        # 更新梯度参数
+        grads = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        # 参数存储，便于查看曲线图
+        with summary_writer.as_default():
+            tf.summary.scalar('train-loss', float(loss), step=step)
+            tf.summary.scalar('test-acc', acc_meter.result().numpy(), step=step)
+            # tf.summary.image('Training data', xx,step=step)
+
+        if step % 1000 == 0:
+            text = acc_meter.result().numpy()
+            print(step, 'loss:', float(loss), 'acc:', acc_meter.result().numpy())
+            acc_meter.reset_states()
+
+    return model
 
 
-#训练
-#
-#
-# def Dl_Run(train_images,train_labels):
-#     x = tf.convert_to_tensor(train_images, dtype=tf.float32) / 255
-#     db = tf.data.Dataset.from_tensor_slices((x, train_labels))
-#     db = db.batch(100).repeat(20)
-#
-#     model = keras.Sequential([layers.Dense(256, activation='relu'),
-#                               layers.Dense(128, activation='relu'),
-#                               layers.Dense(10)])
-#
-#     model.build(input_shape=(None, 28 * 28))
-#     model.summary()
-#     optimizer = optimizers.SGD(lr=0.02)
-#     acc_meter = keras.metrics.Accuracy()
-#     summary_writer = tf.summary.create_file_writer('tf_log')
-#     text = ''
-#
-#     for step, (xx, yy) in enumerate(db):
-#
-#         with tf.GradientTape() as tape:
-#             # 图像样本大小重置(-1, 28*28)
-#             xx = tf.reshape(xx, (-1, 28 * 28))
-#             # 获取输出
-#             out = model(xx)
-#             # 实际标签转为onehot编码
-#             y_onehot = tf.one_hot(yy, depth=10)
-#             # 计算误差
-#             loss = tf.square(out - y_onehot)
-#             loss = tf.reduce_sum(loss / xx.shape[0])
-#         # 更新准备率
-#         acc_meter.update_state(tf.argmax(out, axis=1), yy)
-#         # 更新梯度参数
-#         grads = tape.gradient(loss, model.trainable_variables)
-#         optimizer.apply_gradients(zip(grads, model.trainable_variables))
-#         # 参数存储，便于查看曲线图
-#         with summary_writer.as_default():
-#             tf.summary.scalar('train-loss', float(loss), step=step)
-#             tf.summary.scalar('test-acc', acc_meter.result().numpy(), step=step)
-#             # tf.summary.image('Training data', xx,step=step)
-#
-#         if step % 1000 == 0:
-#             text = acc_meter.result().numpy()
-#             print(step, 'loss:', float(loss), 'acc:', acc_meter.result().numpy())
-#             acc_meter.reset_states()
-#
-#     return text
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,77 +1,38 @@
 import sys
-import tensorflow as tf
-from tensorflow import keras
-from keras import layers, optimizers, datasets
-
+import time
 import Deeplearning
-import UIMain
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import QApplication, QWidget
+from UIMain import Ui_MainWindow
 
 
-# 版本：1.0.0.4
-# 更新日期：20230209
+# 版本：1.0.0.5
+# 更新日期：20230212
 
-class DetThread(QThread):
+class Myclass(QWidget, Ui_MainWindow):
     def __init__(self):
-        super(DetThread, self).__init__()
-        # 这里补充参数
+        super(Myclass, self).__init__()
+        self.init()
 
-    def Run(self):
-        try:
-            # 1:加载数据集&&数据集预处理
-            deeplearning = Deeplearning.DeepLearningClass
-            (train_images, train_labels), (test_images, test_labels) = deeplearning.load_data('MNIST/')
-            # 这里对数据集进行类型转换，统一格式放进模型
-            x = tf.convert_to_tensor(train_images, dtype=tf.float32) / 255
-            # 数据集与标签绑定
-            db = tf.data.Dataset.from_tensor_slices((x, train_labels))
+    # 初始化
+    def init(self):
+        self.setupUi(self)
+        self.textEdit.append('软件初始化成功')
+        self.setWindowTitle('基于特征识别的手写数字识别')
+        self.Button_LoadData.clicked.connect(self.loaddata)
 
-            # 2：创建模型
-            model = keras.Sequential([layers.Dense(256, activation='relue='),
-                                      layers.Dense(128, activation='relue='),
-                                      layers.Dense(10)])
-            model.build(input_shape=(None, 28 * 28))
-            model.summary(None, None, None, True, True, None)
+    # 数据加载
+    def loaddata(self):
+        Deeplearning.load_data()
+        self.Writelog('数据集加载成功')
 
-            optimizer = optimizers.SGD(lr=0.01)
-            acc_meter = keras.metrics.Accuracy()
-            summary_writer = tf.summary.create_file_writer('tf_log')
-
-            for step, (xx, yy) in enumerate(db):
-                with tf.GradientTape() as tape:
-                    # 图像样本大小重置(-1, 28*28)
-                    xx = tf.reshape(xx, (-1, 28 * 28))
-                    # 获取输出
-                    out = model(xx)
-                    # 实际标签转为onehot编码
-                    y_onehot = tf.one_hot(yy, depth=10)
-                    # 计算误差
-                    loss = tf.square(out - y_onehot)
-                    loss = tf.reduce_sum(loss / xx.shape[0])
-                # 更新准备率
-                acc_meter.update_state(tf.argmax(out, axis=1), yy)
-                # 更新梯度参数
-                grads = tape.gradient(loss, model.trainable_variables)
-                optimizer.apply_gradients(zip(grads, model.trainable_variables))
-                # 参数存储，便于查看曲线图
-                with summary_writer.as_default():
-                    tf.summary.scalar('train-loss', float(loss), step=step)
-                    tf.summary.scalar('test-acc', acc_meter.result().numpy(), step=step)
-                    # tf.summary.image('Training data', xx,step=step)
-
-                if step % 1000 == 0:
-                    text = acc_meter.result().numpy()
-                    print(step, 'loss:', float(loss), 'acc:', acc_meter.result().numpy())
-                    acc_meter.reset_states()
-        except Exception as e:
-            return e
+    # 写log
+    def Writelog(self, str):
+        mes = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ':' + str
+        self.textEdit.append(mes)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    uimain = UIMain.Ui_MainWindow()
-    uimain.setupUi(MainWindow)
-    MainWindow.show()
+    Mc = Myclass()
+    Mc.show()
     sys.exit(app.exec_())
